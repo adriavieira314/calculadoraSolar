b6a363ff6058be50a08bd3034acf091c3e5a75b7
Adria Vieira <adriavieira731@gmail.com>
2020-12-06 19:14:00 -0400

    deploy app to heroku

---
+++ public/js/controllers/controller.js
@@ -0,0 +1,192 @@
+var app = angular.module('CalculadoraSolar', ['ui.utils.masks']);
+
+app.controller('Calculadora', ['$scope', function($scope) {
+	$scope.inputCEP = "";
+	$scope.inputValor = "";
+    $scope.cdInput = "cep";
+    $scope.mascara = "0";
+    $scope.energiaText = "";
+    $scope.areaText = "";
+    $scope.paineisText = "";
+    $scope.potPicoText = "";
+    $scope.grupo = "Grupo";
+
+    // listas para os selects
+    $scope.energiaOuValor = [
+        {id: 1, titulo: "Consumo (kWh)"},
+        {id: 2, titulo: "Valor em Reais"}
+    ]
+
+    $scope.potenciaPainel = [
+        {id: 1, potencia: "335W"},
+        {id: 2, potencia: "410W"},
+        {id: 3, potencia: "440W"}
+    ]
+
+    $scope.cep = function() {
+        $scope.mascara = "0000-000";
+        $scope.cdInput = "cep";
+    }
+
+    // funcoes para quando o select mudar
+    $scope.valor = function(id) {
+        
+        if ($scope.selectedValor.id === 1) {
+            $scope.cdInput = "kwh";
+            $scope.mascara = "Consumo (kWh)";
+            $scope.inputValor = "";
+
+            // var timer = null;
+            // $('#valorInput').keyup(function(){
+            //     clearTimeout(timer); 
+            //     timer = setTimeout($scope.energia = $scope.inputValor, 400);
+            //     timer = setTimeout(calculoPotenciaPico, 500);
+            // });
+
+        } else {
+            $scope.cdInput = "reais";
+            $scope.inputValor = "";
+            $scope.mascara = "R$";
+            //aguarda 400 milisegundos depois do usuario terminar de digitar para fazer o calculo
+            // var timer = null;
+            // $('#valorInput').keyup(function(){
+            //     clearTimeout(timer);
+            //     timer = setTimeout(calculoEnergia, 400);
+            //     timer = setTimeout(calculoPotenciaPico, 500);
+            // });
+        }
+
+    }
+
+    $scope.painel = function(id) {
+        if ($scope.selectedPotPainel.id === 1) {
+            $scope.potPainel = 335/1000;
+            
+            // setTimeout(calculoQtdPaineis, 400);
+            // setTimeout(calculoArea, 500);
+
+        } else if ($scope.selectedPotPainel.id === 2) {
+            $scope.potPainel = 410/1000;
+
+            // setTimeout(calculoQtdPaineis, 400);
+            // setTimeout(calculoArea, 500);
+
+        } else {
+            $scope.potPainel = 440/1000;
+
+            // setTimeout(calculoQtdPaineis, 400);
+            // setTimeout(calculoArea, 500);
+        }
+    }
+    //fim
+
+    $scope.resultado = function() {
+        calculoEnergia($scope.selectedValor.id);
+        calculoPotenciaPico();
+        calculoQtdPaineis();
+        calculoArea();
+        calculoPotenciaPico();
+        valorInvestimento();
+        grupo();
+        
+        $scope.energiaText = $scope.energia;
+        $scope.potPicoText = $scope.potPico;
+        $scope.paineisText = $scope.paineis; 
+        $scope.areaText = $scope.area;
+    }
+
+    $scope.apagar = function() {
+        $scope.energiaText = "";
+        $scope.energia = "";
+        $scope.potPicoText = "";
+        $scope.potPico = "";
+        $scope.paineisText = "";
+        $scope.paineis = ""; 
+        $scope.areaText = "";
+        $scope.area = "";
+        $scope.inputValor = "";
+        $scope.cdInput = "cep";
+        $scope.mascara = "0";
+        $scope.grupo = "Grupo";
+        $scope.investimento = "";
+        $scope.selectedValor = "";
+        $scope.selectedPotPainel = "";
+    }
+
+    calculoEnergia = function(id) {
+        if ($scope.selectedValor.id === 1) {
+            $scope.energia = $scope.inputValor;
+        } else {
+            var energia = $scope.inputValor/0.92;
+            $scope.energia = Math.floor(energia);
+        }
+    }
+    
+    calculoPotenciaPico = function() {
+        $scope.potPico = $scope.energia/(30 * 4.33 * 0.80);
+    }
+
+    calculoQtdPaineis = function() {
+        var painel = $scope.potPico/$scope.potPainel;
+        $scope.paineis = Math.floor(painel);
+        if ($scope.paineis %  2 === 1) {
+            $scope.paineis++;
+        }
+        console.log($scope.paineis);
+    }
+
+    calculoArea = function() {
+        $scope.area = 2.03 * $scope.paineis;
+    }
+
+    grupo = function() {
+        if ($scope.potPico >= 104) {
+            $('#modalGrupoA').modal('show');
+            texto = "Sugerimos que você seja cliente do Grupo A, entre em contato com a engenharia.";
+            $(".modal-body").html("");
+			$(".modal-body").append('<p style="margin-left: 20px; font-size: 15px; font-weight: bold;">' + texto + '</p>');
+            $scope.grupo = "Grupo A";
+        } else {
+            $scope.cdInput = "grupo"
+            $scope.grupo = "Grupo B";
+        }
+    }
+
+   valorInvestimento = function() {
+        if ($scope.potPico > 0 && $scope.potPico < 20) {
+            $scope.investimento = 2.000 + 4.346 + 25.000;
+            // $scope.inputValor = $scope.investimento;
+        }
+
+        if ($scope.potPico >= 20 && $scope.potPico < 25) {
+            $scope.investimento = 2.000 + 8.406 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+
+        if ($scope.potPico >= 25 && $scope.potPico < 35) {
+            $scope.investimento = 2.500 + 10.098 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+
+        if ($scope.potPico >= 35 && $scope.potPico < 46) {
+            $scope.investimento = 2.500 + 12.337 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+
+        if ($scope.potPico >= 46 && $scope.potPico < 57) {
+            $scope.investimento = 3.500 + 14.670 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+
+        if ($scope.potPico >= 57 && $scope.potPico < 88) {
+            $scope.investimento = 4.000 + 18.090 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+
+        if ($scope.potPico >= 88 && $scope.potPico < 104) {
+            $scope.investimento = 4.500 + 23.588 + 25.000;
+            // $scope.inputValor = $scope.investimento;        
+        }
+    }
+    
+}]);
