c2f101828dd598b29a482aa9cc21ce192d17d96e
Adria Vieira <adriavieira731@gmail.com>
2020-12-23 14:40:53 -0400

    solucao #7: bug do iphone - option em branco do select

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -335,7 +335,7 @@ app.controller('Calculadora', ['$scope', '$http', function($scope, $http) {
     
     //Para dispostivos moveis
     $(document).ready(function(){
-        $("select").find("option").eq(0).remove();
+        $("selects").find("option").eq(0).remove();
         var tam = $(window).width();
       
         if (tam <= 770){
